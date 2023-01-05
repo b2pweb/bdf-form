ea4e6c51cdc6301dd9ad36221941a3da596b44db
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2023-01-05 10:05:05 +0100

    ci: Fix locale value

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,5 +8,5 @@ ini_set("session.use_only_cookies",0);
 ini_set("session.use_trans_sid",1);
 @session_start();
 
-Locale::setDefault('en-EN');
+Locale::setDefault('en_US');
 date_default_timezone_set('Europe/Paris');
