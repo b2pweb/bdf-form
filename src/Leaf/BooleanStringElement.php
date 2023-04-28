c3dff104db609c7e1325547438590fbc09574f66
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2023-04-28 15:57:55 +0200

    feat: Handle boolean string element (#FRAM-115)

---
+++ src/Leaf/BooleanStringElement.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Bdf\Form\Leaf;
+
+use function filter_var;
+use function is_bool;
+
+/**
+ * Element for boolean value using string representation
+ *
+ * A boolean string is considered as true when the value is on of the following: "true", "on", "yes", "1"
+ * The value is considered as false when the value is on of the following: "false", "off", "no", "0"
+ * For any other value, the value is considered as null
+ *
+ * Note: Case and space are ignored, so "True", "ON", " yes " are valid values
+ *
+ * @see BooleanElementBuilder::booleanString() For build the element
+ */
+class BooleanStringElement extends AbstractBooleanElement
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @return bool|null
+     */
+    protected function toPhp($httpValue): ?bool
+    {
+        if ($httpValue === null) {
+            return null;
+        }
+
+        if (is_bool($httpValue)) {
+            return $httpValue;
+        }
+
+        /** @var bool|null */
+        return filter_var($httpValue, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function toHttp($phpValue): ?string
+    {
+        if ($phpValue === null) {
+            return null;
+        }
+
+        return $phpValue ? 'true' : 'false';
+    }
+}
