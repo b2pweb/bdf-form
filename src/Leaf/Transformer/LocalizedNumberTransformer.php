b85b02b710118aa63e81d06ddd89aeecddab5db7
Vincent QUATREVIEUX <vquatrevieux@b2pweb.com>
2025-01-02 14:07:33 +0100

    ci: fix psalm error on numeric string instead of actual numeric value

--- src/Leaf/Transformer/LocalizedNumberTransformer.php
+++ src/Leaf/Transformer/LocalizedNumberTransformer.php
@@ -9,6 +9,8 @@ use InvalidArgumentException;
 use Locale;
 use NumberFormatter;
 
+use function is_string;
+
 /**
  * Transformer localized string number to native PHP number (int or double)
  *
@@ -76,6 +78,11 @@ class LocalizedNumberTransformer implements TransformerInterface
 
         if (!is_numeric($value)) {
             throw new InvalidArgumentException('Expected a numeric or null.');
+        }
+
+        // Formatter only supports number values, so we cast to float
+        if (is_string($value)) {
+            $value = (float) $value;
         }
 
         $formatter = $this->getNumberFormatter();
